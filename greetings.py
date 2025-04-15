6cd47ecd3b06ea523c74d02a9b0a26a624a6c366
rajeshpatil1998 <rajesh.patil@sail-analytics.com>
2025-04-15 09:20:15 +0530

    another change

--- greetings.py
+++ greetings.py
@@ -1,5 +1,5 @@
 def greet(name):
   """Greets the person passed in as a parameter this is branch 1."""
-  print(f"Welcome to main, {name}!")
+  print(f"Welcome to main changes, {name}!")
 
 greet("Vicky")
