5ab9c5296d326aa903cc9b8c96ad2952e2a3260a
rajeshpatil1998 <rajesh.patil@sail-analytics.com>
2025-04-15 09:29:10 +0530

    changes for merging branch

--- greetings.py
+++ greetings.py
@@ -1,5 +1,5 @@
 def greet(name):
-  """Greets the person passed in as a parameter this is branch 1."""
+  """Greets the person passed in as a parameter this is branch 1 change to main branch"""
   print(f"Welcome to main changes, {name}!")
 
 greet("Vicky")
