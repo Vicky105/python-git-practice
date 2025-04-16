99c82f296e2af62b84bedce0e62577f5a4cf5d69
Vicky105 <vicky1vlr@gmail.com>
2025-04-16 14:32:21 +0530

    welcoming github

--- greetings.py
+++ greetings.py
@@ -1,6 +1,6 @@
 def greet(name):
   """Greets the person passed in as a parameter master branch"""
   """Greets the person passed in as a parameter branch1"""
-  print(f"Welcome to main changes, {name}!")
+  print(f"Welcome to GitHub Repo changes, {name}!")
 
 greet("Vicky")
